b500acc86fa6b5597669483fd105e8ded608856f
t h <taif@gmail.com>
2022-05-15 21:01:33 +0300

    update firstLab.py file

---
+++ FirstLab.py
@@ -0,0 +1,12 @@
+# Define a variable that has a value of your favourite football team.
+footballTeam="Juventus"
+
+# Define a variable “x“  has an integer value and a variable “y” has a float value.
+x , y = 100 , 99.99     # or x:int =100 , y:float=99.99
+
+# Assign One Value to Multiple Variables (choose random names & values).
+name1 = name2 = "Taif"
+
+# Assign multiple Values to Multiple Variables (choose random names & values).
+yourName , fatherName , FamilyName = "Taif" , "Hammad" ,"Alanazi"
+
