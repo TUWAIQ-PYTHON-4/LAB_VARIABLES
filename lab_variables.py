ba6845890bfa030e2b9faa6febd1dca3bf26bd84
NOURAH <nourahsaadan@gmail.com>
2022-05-15 19:38:34 +0300

    Added the lab_variables.py file

---
+++ lab_variables.py
@@ -0,0 +1,12 @@
+# LAB_VARIABLES
+
+
+# Define a variable that has a value of your favourite football team.
+team = 'Juventus'
+# Define a variable “x“  has an integer value and a variable “y” has a float value.
+x = 1
+y = 1.1
+# Assign One Value to Multiple Variables (choose random names & values).
+var1 = var2 = 'Hello'
+# Assign multiple Values to Multiple Variables (choose random names & values).
+var3, var4 = 3,4
