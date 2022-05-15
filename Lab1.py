fd21c9a46b711a48d5fcca553c3c1d0048416629
Aaboassoud <a.aboassoud@gmail.com>
2022-05-15 17:10:56 +0300

    Lab1

---
+++ Lab1.py
@@ -0,0 +1,5 @@
+my_team = "Etihad"
+x = 24
+y = 76.88
+one_to_multi1 = one_to_multi2 = one_to_multi3 = "Hello python!"
+multi_to_multi1 , multi_to_multi2 , multi_to_multi3 = "Adnan" , 24 , "Makkah"   
