86c9f1ad61da1245a45f25f8cd41b7e8026aa9e0
noura <noura@gmail.com>
2022-05-15 20:27:51 +0300

    Success update on LAB_VARIABl

---
+++ LAB_VARIABL.py
@@ -0,0 +1,14 @@
+#Define a variable that has a value of your favourite football team.
+favourite_football=["Alnaser","Alhlal"]
+print("my favourite football:",favourite_football)
+#Define a variable “x“  has an integer value and a variable “y” has a float value.
+x=1
+y=1.2
+print("value x:",x,"value y:",y)
+#Assign One Value to Multiple Variables (choose random names & values).
+Noura = Alnafe = "Nationality:Sadui Arabia"
+print(Noura)
+print(Alnafe)
+#Assign multiple Values to Multiple Variables (choose random names & values).
+NouraAge, SaadAge = "23", "50"
+print("Noura Age is:",NouraAge ,"& Saad age is:",SaadAge) 
