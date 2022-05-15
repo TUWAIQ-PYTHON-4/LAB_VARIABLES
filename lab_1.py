d038dc4de6a78c056dc844d7505bf8fdb1e58182
wafa-alrizqi <wafa.alrizqi@gmail.com>
2022-05-15 20:43:05 +0300

    lab one solved ,  wafa ali

---
+++ lab_1.py
@@ -0,0 +1,13 @@
+# Define a variable that has a value of your favourite football team.
+favourite_team = 'Alhilal SFC'
+
+# Define a variable “x“ has an integer value and a variable “y” has a float value.
+x = 100 
+y = 100.00
+
+# Assign One Value to Multiple Variables (choose random names & values).
+va1_1 = val_2 = val_3 = 'same value'
+
+# Assign multiple Values to Multiple Variables (choose random names & values).
+num_1 , num_2, num_3 = 1 , 2 , 3
+
