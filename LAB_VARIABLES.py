93ee7ed088e39d5455da59e80693106a4c87a980
RubaALmohya <ruba12399@gmail.com>
2022-05-15 21:00:40 +0300

    added python code for this lab

---
+++ LAB_VARIABLES.py
@@ -0,0 +1,13 @@
+#Define a variable that has a value of your favourite football team.
+FootballTeam = 'ALHILAL'
+#Define a variable “x“ has an integer value and a variable “y” has a float value.
+x = 20
+y = 3.6
+#Assign One Value to Multiple Variables (choose random names & values).
+Var1 = Var2 = Var3 = "TUWAIO PYTHON"
+print(Var1)
+print(Var3)
+#Assign multiple Values to Multiple Variables (choose random names & values).
+Var11 , Var22 , Var33 = "PYTHON" ,"LAB" ,"VARIABLES"
+print(Var11)
+print(Var22)
