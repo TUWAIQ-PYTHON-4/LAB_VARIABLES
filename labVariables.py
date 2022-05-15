b61d59f5f0b62664ab6cd155391c8953a749fb1c
jawaher-alqotym <jwahralqtym@gmail.com>
2022-05-15 20:21:46 +0300

    add file lab variariables

---
+++ labVariables.py
@@ -0,0 +1,16 @@
+# Define a variable that has a value of your favourite football team.
+favourite_team = 'Al Hilal'
+
+# Define a variable “x“ has an integer value and a variable “y” has a float value.
+x: int = 100
+y: float = 99.99
+# print(type(x), type(y))
+
+# Assign One Value to Multiple Variables (choose random names & values).
+first_name = last_name = 'anonymous'
+# print(first_name, last_name)
+
+# Assign multiple Values to Multiple Variables (choose random names & values).
+city, street, house_num = 'Riyadh', 'safaqes', 7715 
+# print(city, street, house_num)
+
